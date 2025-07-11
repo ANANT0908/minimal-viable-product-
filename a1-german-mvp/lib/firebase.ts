217017d1790d9b649a0714c86c4932dc4072f289
Anant Verma <anant.verma0908@gmail.com>
2025-07-11 23:55:39 +0530

    add logs

--- a1-german-mvp/lib/firebase.ts
+++ a1-german-mvp/lib/firebase.ts
@@ -8,7 +8,7 @@ const firebaseConfig = {
   projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
   appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
 };
-
+console.log(firebaseConfig,"firebaseConfig")
 
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
